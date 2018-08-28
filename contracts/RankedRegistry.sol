3079984f58413422b1a1a856b9e869d767969092
quantum13 <quant13@gmail.com>
2018-08-28 17:02:39 +0300

    SD-357 contract skeleton

---
+++ contracts/RankedRegistry.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.4.24;
+pragma experimental ABIEncoderV2;
+
+import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
+
+/**
+ * Based on Registry contract by skmgoldin
+ * https://github.com/skmgoldin/tcr
+ *
+ * Original license: https://github.com/skmgoldin/tcr/blob/master/LICENSE.txt (Apache-2.0)
+ */
+
+
+contract RankedRegistry is Ownable /*temporary, for tests*/ {
+
+    struct DappMeta {
+        string name;
+        uint256 metatype;
+        string metadata;
+    }
+
+    mapping (bytes32 => DappMeta) dapps;
+    bytes32[] dappsIds;
+
+    function addDappMetaTemp(string _name, uint _metatype, string _metadata) public onlyOwner {
+        bytes32 id = keccak256(abi.encodePacked(_name, _metatype, _metadata));
+
+        dappsIds.push(id);
+        dapps[id] = DappMeta(
+            _name,
+            _metatype,
+            _metadata
+        );
+    }
+
+}
