0c95b4d1495820a88b60dbdf1d9f472ea5af70ab
Boogerwooger <sergey@mixbytes.io>
2018-08-29 17:16:10 +0000

    working example of uploading and reading data from registry contract

--- utils/registry-updater/updater.py
+++ utils/registry-updater/updater.py
@@ -35,36 +35,77 @@ def main(arguments):
 
     # print('get dapps by id: {}'.format(registry.getDappbyId(1)))
 
-    # dapps_mix = {}
-    # c1 = 0
-    # with open('/tmp/dapps_dict_all', 'r') as json_file:
-    #     dapps_mix = json.load(json_file)
-    #     for d in dapps_mix:
-    #         if c1 >= 1:
-    #             break
-    #         c1 = c1 + 1
-    #         dapp = dapps_mix[d]
-    #         registry.functions.addDappMetaTemp("" + dapp['name'], 1, json.dumps(dapp)).call()
-    
-    name = "XYZ"
-    metatype = 1
-    metadata = json.dumps({'jopa': 1})
-    
-    # print(dapp_id)
-    res = registry.functions.addDappMetaTemp(name, metatype, metadata).call()
-    print(repr(res))
-    # print(registry.functions.getDAppById.call(dapp_id))
+    dapps_crawler = {}
+    dapps_registry = {}
+    c1 = 0
+    MAX_DAPPS = 5
+    with open('/tmp/dapps_dict_all', 'r') as json_file:
+        dapps_crawler = json.load(json_file)
+        for d in dapps_crawler:
+            c1 = c1 + 1
+            if c1 >= MAX_DAPPS:
+                break
+            dapp = None
+            try:
+                dapp = process_smartz_dapp(dapps_crawler[d])
+            except Exception as e:
+                print("[ERROR] Error processing dapp from crawler: {}".format(repr(e)))
+                continue
 
-    # from contract source: 
+            tx_hash = registry.functions.addDappMetaTemp(dapp['name'], 1, json.dumps(dapp)).transact()
+            web3.eth.waitForTransactionReceipt(tx_hash)
+
+    print("[DEBUG] {} dapps uploaded to contract".format(c1))
+    # now lets check that all dapps are uploaded correctly
+
+    dapps_ids = registry.functions.getDAppsIds().call()
+
+    for dapp_id in dapps_ids:
+        dapp = registry.functions.getDAppById(dapp_id).call()
+        print(repr(dapp))
+
+    # from contract source: [HZ] doesn't work
     # bytes32 id = keccak256(abi.encodePacked(_name, _metatype, _metadata));
-    abi_encoded = registry.encodeABI(fn_name='addDappMetaTemp', args=[name, metatype, metadata])
-    dapp_id = web3.sha3(hexstr=abi_encoded[2:])
-    print(repr(dapp_id))
+    # abi_encoded = registry.encodeABI(fn_name='addDappMetaTemp', args=[name, metatype, metadata])
+    # dapp_id = web3.sha3(hexstr=abi_encoded[2:])
 
     print("[FINISHED]")
 
 
+# taken from crawler - the main function to validate dapp, ready for uploading into registry contract
+def process_smartz_dapp(dapp):
+    # copies only needed data from dapps, came from crawler
 
+    result_dapp = {}
+    mandatory_fields = ('name', 'url')
+    for i in mandatory_fields:
+        if dapp.get(i) is None:
+            raise ValueError("Empty mandatory field '{}'(is None):".format(i))
+        if isinstance(dapp[i], list) == True and len(dapp[i]) == 0:
+            raise ValueError("Empty mandatory array '{}'".format(i))
+        result_dapp[i] = dapp[i]
+
+    # DApp must have at leas one address in any network
+    addr_re = re.compile('^0x[0-9a-fA-F]{40}$')
+
+    contracts = dapp.get('contracts')
+    if contracts is None or len(contracts) == 0:
+        raise ValueError("Empty mandatory 'contracts' dict(at least one address in any network must be present")
+
+    for network in contracts:
+        if (len(contracts[network]) == 0):
+            raise ValueError("Empty array of addresses in 'contracts'({})".format(network))
+
+        addresses = contracts[network]
+        if (len(addresses) == 0):
+            raise ValueError("No addresses in 'contracts'({})".format(network))
+
+        for a in addresses:
+            if (not addr_re.match(a)):
+                raise ValueError("Invalid address '{}' in 'contracts'({})".format(a, network))
+    result_dapp['contracts'] = dapp['contracts']
+
+    return result_dapp
 
 def deploy_contract_and_get_instance(web3): # web3 - web3 instance
 
@@ -159,17 +200,15 @@ def deploy_contract_and_get_instance(web3): # web3 - web3 instance
             mapping (bytes32 => DappMeta) public dapps;
             bytes32[] public dappsIds;
 
+            function getDAppsIds() public view returns(bytes32[] ) {
+                return dappsIds;
+            }
+
             function getDAppById(bytes32 _id) public view returns (string name, uint256 metatype, string metadata) {
                 return (dapps[_id].name, dapps[_id].metatype, dapps[_id].metadata);
             }
 
-
-            function _getIdByParams(string _name, uint _metatype, string _metadata) internal returns(bytes32) {
-                bytes32 id = keccak256(abi.encodePacked(_name, _metatype, _metadata));
-                return id;
-            }
-
-            function addDappMetaTemp(string _name, uint _metatype, string _metadata) public onlyOwner returns(bytes32) {
+            function addDappMetaTemp(string _name, uint _metatype, string _metadata) public returns(bytes32) {
                 bytes32 id = keccak256(abi.encodePacked(_name, _metatype, _metadata));
 
                 dappsIds.push(id);
