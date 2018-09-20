28f50774ede4f9125552f8cc466d3bc135784158
Boogerwooger <sergey@mixbytes.io>
2018-09-20 16:00:14 +0000

    temp commit of updater of tcrank contract

--- utils/autoranker/autoranker.py
+++ utils/autoranker/autoranker.py
@@ -32,6 +32,7 @@ class Autoranker(object):
 
 
     def __init__(self, config, private_key):
+        self.config = config
         self.web3 = Web3(Web3.HTTPProvider(config['eth_http_node']))
         # need for Rinkeby network
         self.web3.middleware_stack.inject(geth_poa_middleware, layer=0)
@@ -47,19 +48,48 @@ class Autoranker(object):
         self.address = self.web3.toChecksumAddress("0x{}".format(keccak.hexdigest()[24:]))
         self.tcrank = self.web3.eth.contract(address=self.web3.toChecksumAddress(config['tcrank_address']), abi=config['tcrank_abi'])
 
+    
+    def get_dapp_from_contract(self, dapp_id):
+        try: 
+            r = self.tcrank.functions.getItem(self.to_uint256(dapp_id)).call()
+            return r
+        except Exception as e:
+            print("Dapp with id: {} doesn't exist in contract".format(dapp_id))
+        
+        return None
+ 
     def get_dapps_from_contract(self, dapps):
         their_dapps = {}
         for dapp_id in dapps:
             dapp = dapps[dapp_id]
-            r = self.tcrank.functions.getItem(self.to_uint256(dapp_id)).call()
-            print(r)
+            try: 
+                r = self.tcrank.functions.getItem(self.to_uint256(dapp_id)).call()
+            except Exception as e:
+                print("Dapp with id: {} doesn't exist in contract".format(dapp_id))
+                continue
         
     def load_dapps_info_to_contract(self, dapps):
-        print("not implemented")
+        for dapp_id in dapps:
+            dapp = dapps[dapp_id]
+            existing = self.get_dapp_from_contract(dapp_id)
+            if existing is not None:
+                logger.debug("Dapp [{}] {}, already exists in contract, continue".format(dapp_id, dapp))
+                continue
+
+            dapp = dapps[dapp_id]
+            # est_gaz = self.tcrank.functions.newItemWithRank(self.to_uint256(dapp_id), dapp_our_rank).estimateGas()
+            tx = self.tcrank.functions.newItemWithRank(_id=self.to_uint256(dapp_id), 
+                                                       _rank=self.to_uint256(1)).buildTransaction({
+        						'gas': 1400000,
+        						'gasPrice': self.web3.toWei('10', 'gwei'),
+                                                        'nonce': self.web3.eth.getTransactionCount(self.address)
+                                                        })
+            signed_tx = self.web3.eth.account.signTransaction(tx, private_key=self.private_key)
+            tx_hash = self.web3.eth.sendRawTransaction(signed_tx.rawTransaction) 
+            self.web3.eth.waitForTransactionReceipt(tx_hash)
+            logger.info("Dapp [{}] {}, added to contract".format(dapp_id, dapp))
+        
         return None
-        balance_wei = self.tcrank.functions.balanceOf(self.address).call()
-        balance = Web3.fromWei(balance_wei, 'ether')
-        logger.debug("Init account, all transactions will be fired from address: {}, token's balance: {}".format(self.address, balance))
 
     def push_tcrank_item(self, dapp_id, impulse):
         pass
